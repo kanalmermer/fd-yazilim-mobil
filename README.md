caaabadbefa438bd05142685e7ad46df8c3ab6ff
FD Yazilim <local@fd-yazilim.invalid>
2026-09-09 09:28:18 +0300

    FD Yazilim mobil PWA ilk surum

---
+++ README.md
@@ -0,0 +1,5 @@
+# FD Yazılım Mobil
+
+FD Yazılım web uygulamasını Android ve iOS ana ekranına kurulabilir biçimde sunan PWA kabuğu.
+
+Uygulama verisi bu projede tutulmaz. Mevcut Google Apps Script uygulaması güvenli bir çerçevede açılır.
---
+++ sw.js
@@ -0,0 +1,8 @@
+const SURUM='fd-yazilim-mobil-v1';
+const KABUK=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(SURUM).then(cache=>cache.addAll(KABUK)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==SURUM).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET'||new URL(event.request.url).origin!==self.location.origin)return;
+  event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(SURUM).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))));
+});
